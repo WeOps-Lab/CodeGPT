2974e0845c2e4faeed4c5b2ab25729568217417f
umaru <15875339926@139.com>
2023-11-02 15:47:47 +0800

    任务：添加语言支持和代码审查模板
    - 在gitignore中忽略coverage.txt、dist、**/.DS_Store、bin、release和.idea/文件夹
    - 在prompt/language.go中添加日语语言支持
    - 在prompt/templates/code_review_file_diff.tmpl中添加代码审查模板
    - 在prompt/templates/summarize_file_diff.tmpl中添加文件差异总结模板
    
    Signed-off-by: umaru <15875339926@139.com>

--- prompt/language.go
+++ prompt/language.go
@@ -6,6 +6,7 @@ var languageMaps = map[string]string{
 	"en":    DefaultLanguage,
 	"zh-tw": "Traditional Chinese",
 	"zh-cn": "Simplified Chinese",
+	"ja":    "Japanese",
 }
 
 // GetLanguage returns the language name for the given language code,
